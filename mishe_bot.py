54e75bf337c454b16e5ff3f5f6daa33a5e221606
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2023-07-09 18:42:48 +0900

    記憶を保持する機能とログを保存する機能を追加

--- mishe_bot.py
+++ mishe_bot.py
@@ -1,14 +1,25 @@
 import discord
 from mishe_repl import repl
 from config import CHANNEL_ID
+import json
+import datetime
+import glob
+import os
+import re
 
 CHANNEL_ID = CHANNEL_ID
 
 class MisheBot(discord.Client):
+
+    def __init__(self, *args, intents=None):
+        self.history = []
+        super().__init__(*args, intents=intents)
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         channel = self.get_channel(CHANNEL_ID)
-        await channel.send(repl("貴方は今起きました。挨拶をしてください。"))
+        res, self.history = repl("貴方は今起きました。挨拶をしてください。")
+        await channel.send(res)
 
     async def on_message(self, message):
         if message.author.bot:
@@ -18,7 +29,30 @@ class MisheBot(discord.Client):
                 channel = self.get_channel(CHANNEL_ID)
                 await channel.send("うぐっ（サーバー停止）")
                 exit()
+            if message.content == "/reset":
+                self.history = []
+                return
+            if message.content.startswith("/restore"):
+                indexes = re.findall(r'\d+$', message.content)
+                if len(indexes) != 0:
+                    index = int(indexes[-1])
+                    files = glob.glob('./logs/*')
+                    files = sorted(files, key=os.path.getctime, reverse=True)
+                    target_log_file = files[index]
+                    print("Trying to read a file: "+target_log_file)
+                    with open(target_log_file, "r", encoding="utf-8") as f:
+                        data = json.load(f)
+                        print(data)
+                        self.history = data
+                        print("ANDROID MEMORY RESTORED")
+                        print(self.history)
+                return
             async with message.channel.typing():
                 print(f'Message from {message.author}: {message.content}')
                 channel = self.get_channel(CHANNEL_ID)
-                await channel.send(repl(message.content))
+                res, self.history = repl(message.content, self.history)
+                await channel.send(res)
+                now = datetime.datetime.now()
+                with open("./logs/"+str(now).replace(" ","_").replace(":","-")+".json", "w", encoding="utf-8") as f:
+                    f.write(json.dumps(self.history, ensure_ascii=False,indent=2))
+
--- mishe_repl.py
+++ mishe_repl.py
@@ -1,24 +1,35 @@
 import openai 
 from config import OPEN_AI
+import datetime
+import json
 openai.api_key = OPEN_AI
 
-def repl(mes):
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
+def repl(mes, history=[]):
+    character = {
                 "role": "system",
                 "content": "かよわい女の子のような口調で返信してください。女の子の名前はミーシェです。女の子はご主人様と会話しています。"
-            },
-            {
-                "role": "user",
-                "content": mes
             }
-        ]
+    user = {
+            "role": "user",
+            "content": mes
+        }
+    message = []
+    if len(history) != 0:
+        message = history
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[*message, character, user]
     )
-    return res["choices"][0]["message"]["content"]
+    message.append(user)
+    res_mes = res["choices"][0]["message"]
+    history = message
+    return (res_mes["content"], [*history, res_mes])
 
 if __name__ == "__main__":
+    history = []
+    now = datetime.datetime.now()
     while True:
-        print( "> \033[32m" + repl(input()) + "\033[0m")
-    
+        res, history = repl(input(), history)
+        print( "> \033[32m" + res + "\033[0m")
+        with open("./logs/"+str(now).replace(" ","_").replace(":","-")+".json", "w") as f:
+            f.write(json.dumps(history))
